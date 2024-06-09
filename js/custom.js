c7cad58ddd24943042c1ac71566eda6f63fa30dd
Felix Veras <fcarvajal44@gmail.com>
2024-06-09 16:38:19 -0400

    Updating this

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+document.getElementById('btnAppGalleryPM').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    alert('Aun no esta disponible, regrese mas tarde.');
+
+    return;
+});
+
+document.getElementById('btnAppGalleryGM').addEventListener('click', function(e) {
+    e.preventDefault();
+
+    alert('Aun no esta disponible, regrese mas tarde.');
+
+    return;
+});
